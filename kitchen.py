6fff8bd4d117d599d80fa40f9ca9b847cbdf1846
Eugen <eugeniu.popa@isa.utm.md>
2022-09-13 13:07:08 +0300

    make the kitchen get orders from the dining hall

--- kitchen.py
+++ kitchen.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/order')
+@app.route('/order', methods = ['GET', 'POST'])
+
+def order():
+    data = request.get_json()
+    print(data["order_id"])
 
 def run_kitchen():
-    app.run(host = '0.0.0.0', port = 8000, debug = True)
+    app.run(host = '0.0.0.0', port = 8000, debug = False)
 
 if __name__ == '__main__':
     run_kitchen()

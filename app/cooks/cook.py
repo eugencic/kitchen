1a090b38b7198a15630b7199ec8fbcf649088f3b
Eugen <eugeniu.popa@isa.utm.md>
2022-09-22 18:17:16 +0300

    modify cook_order function with more functionalities, add more cooks

--- app/cooks/cook.py
+++ app/cooks/cook.py
@@ -2,12 +2,22 @@ from threading import Thread
 from components.orders import *
 import requests
 import time
+from components.foods import menu
+import threading
+
+time_unit = 1
 
 # Customized Waiter class extending the Thread class
 class Cook(Thread):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, data, *args, **kwargs):
         # Access methods of the base class
         super(Cook, self).__init__(*args, **kwargs)
+        self.daemon = True
+        self.id = data['id']
+        self.rank = data['rank']
+        self.proficiency = data['proficiency']
+        self.name = data['name']
+        self.catch_phrase = data['catch-phrase']
 
     # Represent the thread's activity
     def run(self):
@@ -18,18 +28,42 @@ class Cook(Thread):
     # Method to cook an order
     def cook_order(self):
         try:
-            order = order_queue.get()
-            number_of_foods = order['items_number']
-            for _ in range(0, number_of_foods):
-                food = ordered_food_queue.get()
-                time.sleep(1)
-                print(f'The cook finished cooking the food nr.{food["food_id"]} from the order nr.{order["order_id"]}.\n')
-                ordered_food_queue.task_done()
-            print(f'The cook finished cooking the order nr.{order["order_id"]}. It took {number_of_foods} time units to prepare it. Sending it to the table.\n')
-            time.sleep(2)
-            # Put the order data in a dictionary
-            payload = dict({'table_id': order['table_id'], 'order_id': order['order_id'], 'items': order['items'], 'priority': order['priority']})
-            requests.post('http://dininghall:3000/distribution', json = payload, timeout = 0.0001)
-            order_queue.task_done()
+            # Get a food item from the queue
+            food = ordered_food_queue.get()
+            # Get the food information from the menu
+            food_info = next((info for info in menu if info['id'] == food['id']), None)
+            # Get the order information of the order where the food is stored
+            food_order_id = None
+            food_order_info = None
+            for id, order in enumerate(order_queue):
+                if order['order_id'] == food['order_id']:
+                    food_order_id = id
+                    food_order_info = order
+            number_of_foods = food_order_info['items_number']
+            # Check if the cook can prepare the food
+            if food_info['complexity'] == self.rank or food_info['complexity'] == self.rank - 1:
+                print(f'{threading.current_thread().name} is cooking the {food_info["name"]}(nr.{food_info["id"]}) for order nr.{food_order_info["order_id"]}')
+                time.sleep(food_info['preparation-time'] * time_unit)
+                order_queue[food_order_id]['cooked_foods'] += 1
+                if order_queue[food_order_id]['cooked_foods'] == number_of_foods:
+                    finished_cooking_time = int(time.time())
+                    print(f'The cook finished cooking the food nr.{food_info["id"]} from the order nr.{order["order_id"]}. It took {finished_cooking_time} time units to prepare it. {self.catch_phrase}\n')
+                    order_queue[food_order_id]['cooking_details'].append({'food_id': food_info['id'], 'cook_id': self.id})
+                    food = ordered_food_queue.task_done()
+                    payload = dict({
+                        'order_id': order_queue[food_order_id]['order_id'], 
+                        'table_id': order_queue[food_order_id]['table_id'], 
+                        'waiter_id': order['waiter_id'],
+                        'items': order['items'], 
+                        'priority': order['priority'],
+                        'wait_time': order['wait_time'],
+                        'pick_up_time': order['pick_up_time'],
+                        'cooking_time': finished_cooking_time - int(order_queue[food_order_id]['receive_time']),
+                        'cooking_details': order_queue[food_order_id]['cooking_details']
+                    })
+                    # requests.post('http://dininghall:3000/distribution', json = payload, timeout = 0.0001)
+                    requests.post('http://localhost:3000/distribution', json = payload, timeout = 0.0001)
+            else:
+                ordered_food_queue.put(food)
         except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
             pass
--- app/kitchen.py
+++ app/kitchen.py
@@ -3,6 +3,7 @@ from components.orders import *
 from components.foods import menu
 from cooks.cook import *
 from threading import Thread
+from components.cooks import cooks
 
 # Create a Flask object called app
 app = Flask(__name__)
@@ -15,37 +16,44 @@ threads = []
 def order():
     data = request.get_json()
     add_order(data)
-    print(f'A new order is received from the waiter. Order nr.{data["order_id"]}\n')
+    print(f'A new order is received from the waiter. Order nr.{data["order_id"]}.\n')
     return {'success': True}
 
 def add_order(order):
     received_order = {
-        'table_id': order['table_id'],
         'order_id': order['order_id'],
+        'table_id': order['table_id'],
+        'waiter_id': order['waiter_id'],
         'items': order['items'],
         'items_number': len(order['items']),
         'priority': order['priority'],
+        'wait_time': order['max_wait'],
+        'pick_up_time': order['pick_up_time'],
+        'receive_time': time.time(),
+        'cooked_foods': 0,
+        'cooking_details': []
     }
-    order_queue.put(received_order)
+    order_queue.append(received_order)
     for id in received_order['items']:
         order_item = next((item for i, item in enumerate(menu) if item['id'] == id), None)
         if order_item is not None:
-            ordered_food_queue.put({'order_id': order['order_id'], 'food_id': order_item['id']})
+            ordered_food_queue.put({'order_id': order['order_id'], 'id': order_item['id']})
     
 def run_kitchen():
     kitchen_thread = Thread(target=lambda: app.run(host = '0.0.0.0', port = 8000, debug = False, use_reloader = False), daemon = True)
     kitchen_thread.start()
-    # Create thread Client
-    cook_thread = Cook()
-    # Add the thread to the array
-    threads.append(cook_thread)
-    
+    for _, cook in enumerate(cooks):
+        # Create thread Client
+        cook_thread = Cook(cook)
+        # Add the thread to the array
+        threads.append(cook_thread)
     # Start the threads
     for thread in threads:
         thread.start()
     # Wait for the threads to complete    
     for thread in threads:
         thread.join()
+        
 
 if __name__ == '__main__':
     run_kitchen()
